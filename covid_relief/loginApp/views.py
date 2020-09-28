9d883838a9ec859d2e2d208a69b9e53c070e91e9
blakedt2280 <57448979+blakedt2280@users.noreply.github.com>
2020-09-28 10:59:13 -0500

    added placeholder sites

--- covid_relief/loginApp/views.py
+++ covid_relief/loginApp/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>hello</h1>')
+    return HttpResponse('<h1>homepage</h1>')
     #return render(request, 'covid_relief/home.html')
+
+def login(request):
+    return HttpResponse('<h1>login</h1>')
+
+def register(request):
+    return HttpResponse('<h1>register</h1>')
