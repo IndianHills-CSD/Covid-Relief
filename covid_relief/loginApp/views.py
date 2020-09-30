023dd560602911cb9f12f43379bf2a022c6c3cfe
david-amos <57191710+david-amos@users.noreply.github.com>
2020-09-30 11:17:08 -0500

    python variables to pass and css styling

--- covid_relief/loginApp/views.py
+++ covid_relief/loginApp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+ 
+ 
 def home(request):
-
+    dummy = {'Active': 28, 'Recovered': 31, 'Uninfected': 62}
     #return HttpResponse('<h1>hello</h1>')
-    return render(request, 'loginApp/home.html')
+    return render(request, 'loginApp/home.html',dummy)
 
 
 def login(request):
