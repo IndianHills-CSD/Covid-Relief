a1531b3383d237ac4f3a3e6a4fc491ce9fdaf39f
blakedt2280 <57448979+blakedt2280@users.noreply.github.com>
2020-10-21 11:13:17 -0500

    new table

--- covid_relief/loginApp/models.py
+++ covid_relief/loginApp/models.py
@@ -8,10 +8,10 @@ class Post(models.Model):
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete =models.CASCADE)
 
-
-
-	
-		
-
 	def __str__(self):
 		return self.title
+
+class UserInfo(models.Model):
+	address = models.TextField(max_length=100)
+	status = models.CharField(max_length=10)
+	userKey = models.ForeignKey(User, on_delete =models.CASCADE)
