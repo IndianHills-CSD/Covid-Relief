f2f3b763f843ff7d75bd993ea4ceff89bcebb8c8
blakedt2280 <57448979+blakedt2280@users.noreply.github.com>
2020-11-03 11:22:53 -0600

    fixed

--- covid_relief/loginApp/views.py
+++ covid_relief/loginApp/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 from .models import UserInfo
+from django.views.generic import CreateView
 from .forms import UserRegisterForm
 # Create your views here.
 
  
 def home(request):
-	all = UserInfo.objects.all()
-	i = UserInfo.objects.filter(status='infected')
-	iLen = len(i)
-	u = UserInfo.objects.filter(status='uninfected')
-	uLen = len(u)
-	r = UserInfo.objects.filter(status='recovered')
-	rLen = len(r)
-	context = {'Posts': Post.objects.all(),'infected': iLen, 'uninfected': uLen, 'recovered': rLen}
-	return render(request, 'loginApp/home.html',context)
+    all = UserInfo.objects.all()
+    i = UserInfo.objects.filter(status='infected')
+    iLen = len(i)
+    u = UserInfo.objects.filter(status='uninfected')
+    uLen = len(u)
+    r = UserInfo.objects.filter(status='recovered')
+    rLen = len(r)
+    context = {'Posts': Post.objects.all(),'infected': iLen, 'uninfected': uLen, 'recovered': rLen}
+    return render(request, 'loginApp/home.html',context)
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def login(request):
@@ -26,21 +36,21 @@ def register(request):
     #return HttpResponse('<h1>register</h1>')
     errmsg = ''
     if request.method == 'POST':
-    	form = UserRegisterForm(request.POST)
-    	address = request.POST.get('address')
-    	
-    	if (form.is_valid() and request.POST.get('address') != ''):
-    		form.save()
-    		username = form.cleaned_data.get('username')
-    		ui = UserInfo(address=request.POST.get('address'),status=request.POST.get('status'),userKey=username)
-    		ui.save()
-    		return redirect('login')
-    	else:
-    		if request.POST.get('address') == '':
-    			errmsg = "must enter an address"
-    		else:
-    			errmsg = ''
+        form = UserRegisterForm(request.POST)
+        address = request.POST.get('address')
+        
+        if (form.is_valid() and request.POST.get('address') != ''):
+            form.save()
+            username = form.cleaned_data.get('username')
+            ui = UserInfo(address=request.POST.get('address'),status=request.POST.get('status'),userKey=username)
+            ui.save()
+            return redirect('login')
+        else:
+            if request.POST.get('address') == '':
+                errmsg = "must enter an address"
+            else:
+                errmsg = ''
     else:
-    	form = UserRegisterForm()
-    	
+        form = UserRegisterForm()
+        
     return render(request, 'loginApp/register.html', {'form': form, 'err' : errmsg})
